ad0158f950c0d510cd0c37720dff541a85f88bcb
vj18 <vibhorjain13@gmail.com>
2015-03-07 15:47:27 +0530

    Submitting all four png files and their corresponding scripts for assignment

---
+++ plot1.R
@@ -0,0 +1,8 @@
+data <- read.table("~/exdata-data-household_power_consumption/household_power_consumption.txt", sep = ";", na.strings = "?", colClass = c(rep("character", 2), rep("numeric", 7)), header = TRUE)
+select <- (data[,1]=="2/2/2007")|(data[,1]=="1/2/2007")
+data <- subset(data, select)
+png("plot1.png", bg = "transparent")
+hist(data$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", ylim = c(0, 1200), main = "Global Active Power")
+dev.off()
+rm(data, select)
+gc()
